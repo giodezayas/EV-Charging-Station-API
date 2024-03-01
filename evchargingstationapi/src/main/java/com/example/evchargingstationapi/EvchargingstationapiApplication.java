de0972cf59e0a2f5cad3e70d60a7c71c44299c3e
Giovanni De Zayas Angel <62956285+giodezayas@users.noreply.github.com>
2024-03-01 00:03:04 -0500

    Add files via upload

---
+++ evchargingstationapi/src/main/java/com/example/evchargingstationapi/EvchargingstationapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.evchargingstationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvchargingstationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvchargingstationapiApplication.class, args);
+	}
+
+}
---
+++ evchargingstationapi/src/test/java/com/example/evchargingstationapi/EvchargingstationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evchargingstationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvchargingstationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
